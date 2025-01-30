645ea15499e2740425ddc65ad5b80058d27526f3
Norberto Merani <nmera003@fiu.edu>
2025-01-29 21:32:56 -0500

    quick js test

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const userAction = async () => {
     const response = await fetch('https://a25714b3o5.execute-api.us-east-1.amazonaws.com/default/userCountFunction');
     const data = await response.json();
-    console.log(data);
+    window.userCount = data.userCount;
   };
   
-  userAction();
+  window.userAction = userAction;
   
