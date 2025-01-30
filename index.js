7ae3a60eecde5b191407ea805ce53d6ebf8a8f8b
Norberto Merani <nmera003@fiu.edu>
2025-01-29 20:45:23 -0500

    slight edit to js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const userAction = async () => {
-    const response = await fetch('https://a25714b3o5.execute-api.us-east-1.amazonaws.com/default');
+    const response = await fetch('https://a25714b3o5.execute-api.us-east-1.amazonaws.com/default/userCountFunction');
     const data = await response.json();
     console.log(data);
   };
