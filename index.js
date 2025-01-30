47e1cb2fb03cc4bb797204a70409630b2c06c589
Norberto Merani <nmera003@fiu.edu>
2025-01-29 22:26:43 -0500

    2nd quick js test

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-const userAction = async () => {
-    const response = await fetch('https://a25714b3o5.execute-api.us-east-1.amazonaws.com/default/userCountFunction');
-    const data = await response.json();
-    window.userCount = data.userCount;
-  };
-  
-  window.userAction = userAction;
+const response = await fetch('https://a25714b3o5.execute-api.us-east-1.amazonaws.com/default/userCountFunction');
+const data = await response.json();
   
